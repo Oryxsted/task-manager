a5560b857b6ab8d0af649dabedeb1a6caf86b20c
oryxsted <oryxsted@icloud.com>
2022-11-16 16:21:09 +0400

    Добавлены вкладки

---
+++ src/components/popup/Popup.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+/**
+ * 
+ * @returns Контейнер модального окна
+ */
+const Popup = () => {
+  return (
+    <div className='popup__bg'>
+        <div className='popup__wrapper'>
+            <div className='popup__content'>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Popup
